73e4f4b8e9cae7b81ef70ef0d5a76d2e195cacac
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2022-06-28 23:35:39 -0700

    Added code written in P5js editor

--- script.js
+++ script.js
@@ -1,125 +1,114 @@
-/* exported setup, draw */
-let seed = 12345;
-
-const grassColor = "#e1ac4a";
-const skyColor = "#cdd8e6";
-const hillColor = "#1e273f";
-const treeColor = "#3d1803";
-const leaveColor = "#233610";
-const sunColor = [254,254,254,80]; // with opacity
+// Variables
+const WaterColor = "#177FD6";
+const KelpColor = "#016031";
+const KelpHighlight = "#068848";
+const RockColor = "#182A52";
+const FishColor1 = "#C3860D";
+const FishColor2 = "#C3860D";
+const FishColor3 = "#C3860D";
 
 function preload() {
-    // runs before setup 
-    // use if you want to load any large files and want to make sure they load before setup()
+
 }
 
 function setup() {
-  createCanvas(800, 400);
-  createButton("reroll").mousePressed(() => seed++);
+  createCanvas(400, 400);
 }
 
+seed = 0;
 function draw() {
-  randomSeed(seed);
-
-  background(100);
-
-  noStroke();
-
-  fill(skyColor);
-  rect(0, 0, width, height / 2);
-
-  // An example of making something respond to the mouse
-  fill(...sunColor);
-  ellipse(mouseX,0,30,30);
-  ellipse(mouseX,0,50,50);
-  ellipse(mouseX,0,100,100);
-  ellipse(mouseX,0,200,200);
-
-  fill(grassColor);
-  rect(0, height / 2, width, height / 2);
-
-  // An example of drawing an irregular polygon
-  fill(hillColor);
-  beginShape();
-  vertex(0, height / 2);
-  const steps = 10;
-  for (let i = 0; i < steps + 1; i++) {
-    let x = (width * i) / steps;
-    let y =
-      height / 2 - (random() * random() * random() * height) / 8 - height / 50;
-    vertex(x, y);
+  //variables
+  seed = 0
+  
+  if(mouseX == 0) {
+    kelpLean = 0;
+  } else {
+    kelpLean = 200 * mouseX/width;
   }
-  vertex(width, height / 2);
-  endShape(CLOSE);
-
-  const trees = 5*random();
-  for (let i = 0; i < trees; i++) {
-    drawLtree();
+  // print(mouseX + ", " + mouseY)
+  
+  // Drawing
+  background(WaterColor);
+  for(i = -width; i < width; i += 30 + NoiseGen(100)) {
+    kelp(i);
   }
-
-  // An example of recursively drawing an L-tree 
-  function drawLtree() {
-    let x = width * random();
-    let y = height/2 + height/8 * random();
-    let s = width/200 + (y - height/2)/2;
-    let jitter = (mouseX - width/2) / width * 2 * Math.PI / 180;
-    drawLtreeBranch(x, y, s, (-90 * Math.PI / 180) + jitter, 0, 5); // this angle points north (0 is east)
-  }  
-
-  function drawLtreeBranch(x, y, s, angle, max_limit, branch_weight) { // s is length of a segment
-    stroke(treeColor);
-    strokeWeight(branch_weight);
-    let v = p5.Vector.fromAngle(angle, s);
-    let vx = v.x;
-    let vy = v.y; 
-    let x1 = x;
-    let y1 = y; 
-    let x2 = x1 + vx;
-    let y2 = y1 + vy;
-    line(x1, y1, x2, y2);
-
-    let new_s = s * 0.7;
-    let new_max = max_limit + random();
-    let new_branch_weight = branch_weight - 1;
-    new_branch_weight = max(new_branch_weight, 1);
-
-    if (max_limit < 3) {
-        if (random() < 1/3) {
-            drawLtreeBranch(x2, y2, new_s, (-35 * Math.PI / 180) + angle, new_max, new_branch_weight);
-        } else if (random() > 1/3) {
-            drawLtreeBranch(x2, y2, new_s, (35 * Math.PI / 180) + angle, new_max, new_branch_weight);
-        } else {
-            drawLtreeBranch(x2, y2, new_s, (-35 * Math.PI / 180) + angle, new_max, new_branch_weight);
-            drawLtreeBranch(x2, y2, new_s, (35 * Math.PI / 180) + angle, new_max, new_branch_weight);
-        }
-        drawLtreeBranch(x2, y2, new_s, angle, new_max, new_branch_weight);
-    }
-    else {
-        if (random() < 1/3) {
-            drawLeave(x2, y2, new_s, (-35 * Math.PI / 180) + angle);
-        } else if (random() > 1/3) {
-            drawLeave(x2, y2, new_s, (35 * Math.PI / 180) + angle);
-        } else {
-            drawLeave(x2, y2, new_s, (-35 * Math.PI / 180) + angle);
-            drawLeave(x2, y2, new_s, (35 * Math.PI / 180) + angle);
-        }
-    }
-
+  for(k = 0; k < NoiseGen(50); k++) {
+    fish(NoiseGen(width*2) - width/2 + kelpLean*0.7, NoiseGen(height));
   }
-
-  function drawLeave(x, y, s, angle) {
-    fill(leaveColor);
-    noStroke();
-    let v = p5.Vector.fromAngle(angle, s);
-    let vx = v.x;
-    let vy = v.y; 
-    let x1 = x;
-    let y1 = y; 
-    let x2 = x1 + vx;
-    let y2 = y1 + vy;
-    line(x1, y1, x2, y2);
-    circle(x2, y2, 3);
-
+  
+  for(f = -width; f < width; f +=  NoiseGen(50)) {
+    rock(f);
   }
 }
 
+
+function kelp(x) {
+  // draw stalk
+  stroke(KelpColor);
+  strokeWeight(3);
+  line(x, height, x + kelpLean, 0);
+  // calc ski==lope and draw leaves
+  slope = (-height) / (kelpLean); 
+  for(j = -50; j < height; j += 5 + NoiseGen(40)) {
+    leaf(x + kelpLean + j/slope - 25, j, 0);
+  }
+  for(j = -50; j < height; j += 5 + NoiseGen(60)) {
+    leaf(x + kelpLean + j/slope - 25, j, 0);
+  }
+  
+  for(j = -50; j < height; j += 5 + NoiseGen(40)) {
+    leaf(x + kelpLean + j/slope - 4, j, 1);
+  }
+  for(j = -50; j < height; j += 5 + NoiseGen(60)) {
+    leaf(x + kelpLean + j/slope - 7, j, 1);
+  }
+}
+
+// makes a leaf at x, y which leans z (1 = right, 0 = left)
+function leaf(x, y, z) {
+  noStroke();
+  w = 30;
+  h = 10;
+  l = 50
+  
+  push();
+  translate(x + w/2, y);
+  if(z == 1) {
+    rotate(radians(NoiseGen(-45) - 15 + kelpLean/10));
+  } else {
+    rotate(radians(NoiseGen(45) + 15 + kelpLean/10));
+  }
+  
+  fill(KelpColor);
+  ellipse(0, 0, w, h);
+  
+  fill(KelpHighlight);
+  ellipse(0, 0, w*0.7, h*0.3);
+  // stroke(0);
+  // line(x + 5, y, x + w - 5, y);
+  
+  pop();
+}
+
+function fish(x, y) {
+  noStroke();
+  w = 25;
+  h = 10;
+  fill(FishColor1);
+  ellipse(x, y, w, h);
+  triangle(x - w*0.8, y - h/2, x - w*0.8, y + h/2, x - w/4, y);
+  // triangle(0, 0, width, height, 0, height);
+  fill(0);
+  circle(x + w*0.35, y, 3);
+}
+
+function rock(x) {
+  fill(RockColor);
+  quad(x, height +10, x + 10 + NoiseGen(40), height+10, x + 10 + NoiseGen(40), height - 10 - NoiseGen(20), x + NoiseGen(10), height - 10 - NoiseGen(20));
+}
+
+// returns a number between 0 and x
+function NoiseGen(x) {
+  seed += 1;
+  return noise(seed) * x;
+}
